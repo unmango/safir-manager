efdcb725ce37f89beb05834ac078b8dacd9c9b19
Erik Rasmussen <ecr.referee@hotmail.com>
2021-05-06 23:18:50 -0500

    Remove protos proj

--- src/Manager/Startup.cs
+++ src/Manager/Startup.cs
@@ -31,8 +31,6 @@ namespace Manager
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
-
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
