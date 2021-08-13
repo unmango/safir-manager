93663f193b2a613477fccdfefe28941024d168b0
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-12 19:00:15 -0500

    WIP

---
+++ src/Safir.Manager/Agents/Agents.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Safir.Manager.Agents
+{
+    internal class Agents : IAgents
+    {
+        private readonly Dictionary<string, IAgent> _agents;
+
+        public Agents()
+        {
+            _agents = new();
+        }
+
+        public IEnumerator<IAgent> GetEnumerator() => _agents.Values.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public IAgent this[string name] => _agents[name];
+    }
+}
