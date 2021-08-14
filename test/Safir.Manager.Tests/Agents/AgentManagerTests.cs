8d6923ae833fbea40336127a9ca191433fc74cce
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-13 23:31:07 -0500

    TESTS

--- test/Safir.Manager.Tests/Agents/AgentManagerTests.cs
+++ test/Safir.Manager.Tests/Agents/AgentManagerTests.cs
@@ -1,7 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Moq.AutoMock;
+using Safir.Manager.Agents;
+using Safir.Manager.Configuration;
+using Xunit;
+
 namespace Safir.Manager.Tests.Agents
 {
     public class AgentManagerTests
     {
-        
+        private readonly AutoMocker _mocker = new();
+        private readonly Mock<IOptionsMonitor<ManagerOptions>> _optionsMonitor;
+        private readonly AgentManager _manager;
+
+        public AgentManagerTests()
+        {
+            _optionsMonitor = _mocker.GetMock<IOptionsMonitor<ManagerOptions>>();
+            _manager = _mocker.CreateInstance<AgentManager>();
+        }
+
+        [Fact]
+        public void Throws_WhenArgsAreNull()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new AgentManager(null!, new Mock<ILogger<AgentManager>>().Object));
+
+            Assert.Throws<ArgumentNullException>(
+                () => new AgentManager(new Mock<IOptionsMonitor<ManagerOptions>>().Object, null!));
+        }
+
+        [Theory]
+        [MemberData(nameof(AgentOptionsTestData))]
+        public void EnumeratesAgents(IEnumerable<AgentOptions> agentOptions, IEnumerable<string> urls)
+        {
+            _optionsMonitor.SetupGet(x => x.CurrentValue)
+                .Returns(new ManagerOptions {
+                    Agents = agentOptions.ToList()
+                });
+
+            var result = _manager.Count();
+
+            Assert.Equal(urls.Count(), result);
+        }
+
+        [Theory]
+        [MemberData(nameof(AgentOptionsTestData))]
+        public void UsesBaseUrlAsName(IEnumerable<AgentOptions> agentOptions, IEnumerable<string> urls)
+        {
+            _optionsMonitor.SetupGet(x => x.CurrentValue)
+                .Returns(new ManagerOptions {
+                    Agents = agentOptions.ToList()
+                });
+
+            foreach (var url in urls)
+            {
+                Assert.NotNull(_manager[url]);
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(AgentOptionsTestData))]
+        public void SetsNameOnProxy(IEnumerable<AgentOptions> agentOptions, IEnumerable<string> urls)
+        {
+            _optionsMonitor.SetupGet(x => x.CurrentValue)
+                .Returns(new ManagerOptions {
+                    Agents = agentOptions.ToList()
+                });
+
+            foreach (var url in urls)
+            {
+                Assert.Equal(url, _manager[url].Name);
+            }
+        }
+
+        public static IEnumerable<object[]> AgentOptionsTestData()
+        {
+            yield return new object[] {
+                new AgentOptions[] {
+                    new() { BaseUrl = "https://example.com" }
+                },
+                new[] { "https://example.com" }
+            };
+
+            yield return new object[] {
+                new AgentOptions[] {
+                    new() { BaseUrl = "https://example.com" },
+                    new() { BaseUrl = "https://example2.com" }
+                },
+                new[] {
+                    "https://example.com",
+                    "https://example2.com"
+                }
+            };
+        }
     }
 }
