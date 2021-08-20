0d12ac1a3d0bc554822068b93c0466ca91b0ae21
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-19 23:03:25 -0500

    Rename existing proxy to client

--- src/Safir.Manager/Agents/AgentClient.cs
+++ src/Safir.Manager/Agents/AgentClient.cs
@@ -1,12 +1,11 @@
 using System;
-using Grpc.Net.ClientFactory;
 using Safir.Agent.Client;
 
 namespace Safir.Manager.Agents
 {
-    internal class AgentProxy : IAgent
+    internal class AgentClient : IAgent
     {
-        public AgentProxy(string name, IFileSystemClient fileSystem, IHostClient host)
+        public AgentClient(string name, IFileSystemClient fileSystem, IHostClient host)
         {
             FileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
             Host = host ?? throw new ArgumentNullException(nameof(host));
