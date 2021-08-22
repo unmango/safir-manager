f474e4811a6f94c76d7f123530c6c38c35780ae3
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-21 21:45:53 -0500

    Fix some stuff and GET IT WORKING OMG

--- src/Safir.Manager/Agents/JsonFileRequestProxy.cs
+++ src/Safir.Manager/Agents/JsonFileRequestProxy.cs
@@ -1,6 +1,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Threading;
@@ -23,7 +24,9 @@ namespace Safir.Manager.Agents
 
         public ValueTask<T?> RequestAsync<T>(string path, object? args = null, CancellationToken cancellationToken = default)
         {
-            return RequestAsync<T>(_root, path, _fileName, args, cancellationToken);
+            return _requestMap.TryGetValue(Key(path, args), out var result)
+                ? new ValueTask<T?>((T?)result)
+                : RequestAsyncCore<T>(_root, path, _fileName, args, cancellationToken);
         }
 
         public async IAsyncEnumerable<T> RequestAsyncEnumerable<T>(
@@ -37,37 +40,44 @@ namespace Safir.Manager.Agents
 
             foreach (var fileSystemEntry in result)
                 yield return fileSystemEntry;
-        } 
-
-        public static ValueTask<T?> RequestAsync<T>(
-            string root,
-            string path,
-            string fileName,
-            object? args = null,
-            CancellationToken cancellationToken = default)
-        {
-            if (Path.EndsInDirectorySeparator(path))
-                path = Path.Combine(path, fileName);
-            
-            var fullPath = Path.Combine(root, path);
-            return _requestMap.TryGetValue(Key(fullPath, args), out var result)
-                ? new ValueTask<T?>((T?)result)
-                : RequestAsyncCore<T>(fullPath, args, cancellationToken);
         }
 
         private static async ValueTask<T?> RequestAsyncCore<T>(
+            string root,
             string path,
+            string fileName,
             object? args,
             CancellationToken cancellationToken = default)
         {
-            if (!File.Exists(path)) return default;
+            var file = ResponseFile(root, path, fileName);
+
+            if (file == null) return default;
             
-            await using var stream = File.OpenRead(path);
+            await using var stream = File.OpenRead(file);
             var result = await JsonSerializer.DeserializeAsync<T>(stream, _serializerOptions, cancellationToken);
             _requestMap.TryAdd(Key(path, args), result);
             return result;
         }
 
         private static string Key(string path, object? args) => $"{path}({args})";
+
+        private static string? ResponseFile(string root, string path, string fileName)
+        {
+            if (!Path.IsPathRooted(root))
+            {
+                var assembly = Assembly.GetExecutingAssembly().Location;
+                root = Path.Join(Path.GetDirectoryName(assembly), root);
+            }
+
+            if (!Directory.Exists(root)) return null;
+
+            var context = Path.Combine(root, path);
+
+            if (File.Exists(context)) return context;
+
+            var file = Path.Combine(context, fileName);
+
+            return File.Exists(file) ? file : null;
+        }
     }
 }
